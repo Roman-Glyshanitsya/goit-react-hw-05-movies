00fffcef7600ca295a4df3b3c5fd6983a0f3d685
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2025-06-18 01:36:47 +0300

    up

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,30 +1,35 @@
 import PropTypes from 'prop-types';
 import s from './MovieDetails.module.css';
 
-export default function MovieDetails({ movie }) {   
-    return (
-        <div className={s.container}>
-            <div className={s.imageWrapper}>
-                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
-            </div>
-            <div>
-                <h1>{movie.title} ({movie.release_date.slice(0,4)})</h1>
-                <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
-                <h2>Overview</h2>
-                <p>{movie.overview}</p>
-                <h2>Genres</h2>
-                {movie.genres.map(g => g.name + ' ')}
-            </div>
-        </div>
-    )
+export default function MovieDetails({ movie }) {
+  return (
+    <div className={s.container}>
+      <div className={s.imageWrapper}>
+        <img
+          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </div>
+      <div className={s.textContainer}>
+        <h1 className={s.movieTitle}>
+          {movie.title} ({movie.release_date.slice(0, 4)})
+        </h1>
+        <p>User Score: {movie.vote_average.toFixed(1)}</p>
+        <h2>Overview</h2>
+        <p>{movie.overview}</p>
+        <h2>Genres</h2>
+        {movie.genres.map(g => g.name + ' ')}
+      </div>
+    </div>
+  );
 }
 
 MovieDetails.propTypes = {
-    movie: PropTypes.shape({
-        poster_path: PropTypes.string,
-        title: PropTypes.string,
-        release_date: PropTypes.string,
-        vote_average: PropTypes.number,
-        overview: PropTypes.string,
-    }).isRequired,
+  movie: PropTypes.shape({
+    poster_path: PropTypes.string,
+    title: PropTypes.string,
+    release_date: PropTypes.string,
+    vote_average: PropTypes.number,
+    overview: PropTypes.string,
+  }).isRequired,
 };
