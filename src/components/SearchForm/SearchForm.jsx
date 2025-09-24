14a4ba04d14564be5ec1a5d98625a9ec1c653cb4
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2025-09-24 23:40:10 +0300

    up

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,46 +1,49 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import s from './SearchForm.module.css';
 import PropTypes from 'prop-types';
 
 export default function MovieSearchForm({ onSubmit }) {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const query = searchParams.get('query');
-    const [searchMovie, setSearchMovie] = useState(query ?? '');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
+  const [searchMovie, setSearchMovie] = useState(query ?? '');
 
-    const handleChange = event => {
-        setSearchMovie(event.currentTarget.value.toLowerCase());
-    };
+  const handleChange = event => {
+    setSearchMovie(event.currentTarget.value.toLowerCase());
+  };
 
-    const handleSubmit = event => {
-        event.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault();
 
-        if (searchMovie.trim() === '') {
-            toast.error('Enter something for search');
-            return;
-        }
-
-        setSearchParams({query: searchMovie})
+    if (searchMovie.trim() === '') {
+      toast.error('Enter something for search');
+      return;
     }
 
-    return(
-        <form className={s.searchForm}>
-            <input
-                type="text"
-                value={searchMovie}
-                name="query"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search movie"
-                className={s.searchFormInput}
-                onChange={handleChange}> 
-            </input>
-            <button className={s.searchFormBtn} type="submit" onClick={handleSubmit}>Search</button>
-        </form>
-    )
+    setSearchParams({ query: searchMovie });
+    onSubmit(searchMovie);
+  };
+
+  return (
+    <form className={s.searchForm} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={searchMovie}
+        name="query"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search movie"
+        className={s.searchFormInput}
+        onChange={handleChange}
+      ></input>
+      <button className={s.searchFormBtn} type="submit">
+        Search
+      </button>
+    </form>
+  );
 }
 
 MovieSearchForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired
+  onSubmit: PropTypes.func.isRequired,
 };
